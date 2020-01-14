dd57bc0eae78807f51536be62e4a024715e28999
Paweł Gajewski <269823@pw.edu.pl>
2020-01-14 20:45:29 +0100

    Add plots collecting data for different types of regulators

--- networkLinearization.m
+++ networkLinearization.m
@@ -5,7 +5,7 @@ a = zeros(1,na);
 b = zeros(1,nb);
 if strcmp(fun, 'tanh')
     for l=1:na
-        a(l) =-sum(w2*(tan_deriv(w10 + w1*x0).*w1(:,l)));
+        a(l) =-sum(w2*(tan_deriv(w10 + w1*x0).*w1(:,nb-tau+1+l)));
     end
     for l=1:nb
         if l < tau
